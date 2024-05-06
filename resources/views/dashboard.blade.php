50edeab872bbe22946a57d8746b77a2a9aa632ed
ronzxc0024 <aaronlegaspi0024@gmail.com>
2024-05-06 14:43:13 +0800

    changing initial serving page to login

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
                     </div>
                     <hr />
                     @if(Session::has('success'))
-                    <div class="alert alert-success" role="alert">
+                    <div class="alert alert-success" role="alert"> 
                         {{ Session::get('success') }}
                     </div>
                     @endif
